7986d8de2d301c2d1bcde7349c8901e0b4887c40
basukimuhammad <116169432+basukimuhammad@users.noreply.github.com>
2024-11-23 16:35:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require("path");
 const Fastify = require('fastify');
 const cerpen = require('./api/timeApi.js'); // Impor fungsi cerpen dari timeApi.js
 
 const app = Fastify({
   logger: true
+});
+
+// Konfigurasi untuk file statis
+app.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'api'),
 });
 
 // Endpoint utama
@@ -30,4 +37,7 @@ app.get('/cerpen/:category', async (req, res) => {
 });
 
 // Menentukan port server
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port : ${port}`);
+});
